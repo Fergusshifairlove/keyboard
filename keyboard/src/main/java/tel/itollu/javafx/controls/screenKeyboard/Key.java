02299a7bb2815fcca0ed962e773d632e24b390e0
itollu <imt@server.fake>
2013-10-11 13:35:47 +0400

    Add Key.java to implement custom JavaFX control that represents screen keyboard button

---
+++ keyboard/src/main/java/tel/itollu/javafx/controls/screenKeyboard/Key.java
@@ -0,0 +1,17 @@
+package tel.itollu.javafx.controls.screenKeyboard;
+
+import javafx.scene.control.Button;
+
+
+public class Key extends Button {
+
+    private String label;
+    private int scanCode;
+
+    public Key(String label, int scanCode) {
+        super();
+        this.label = label;
+        this.scanCode = scanCode;
+    }
+
+}
